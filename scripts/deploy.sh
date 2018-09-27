57796aab527e221d336deac847f2dedb905930e8
Brendan J. Baker <brendan@brendanbaker.com>
2018-09-27 18:27:44 -0400

    React to POM changes in deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,10 @@ set -euxo pipefail
 
 path_script=$(readlink -f "$0")
 path_root=$(dirname $(dirname "$path_script"))
+project=jbakery-arguments
 
-pushd $path_root/jbakery-arguments
+pushd $path_root/$project
 export GPG_TTY=$(tty)
 export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
-mvn release:clean release:prepare release:clean
+mvn clean deploy
 popd
