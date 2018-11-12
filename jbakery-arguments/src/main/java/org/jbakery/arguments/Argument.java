dc0035830d5779dc8e06929b5df56e5a8482dc84
Brendan J. Baker <brendan@brendanbaker.com>
2018-11-12 00:43:29 -0500

    Provide Argument.notNegative() facilities for byte, short, int, and long primitive types.

--- jbakery-arguments/src/main/java/org/jbakery/arguments/Argument.java
+++ jbakery-arguments/src/main/java/org/jbakery/arguments/Argument.java
@@ -4,6 +4,46 @@ public class Argument
 {
 	private Argument() { }
 
+	public static byte notNegative(byte argument, String argumentName)
+	{
+		Argument.notNull(argumentName, "argumentName");
+
+		if (argument < 0)
+			throw new NegativeArgumentException(argumentName);
+
+		return argument;
+	}
+
+	public static short notNegative(short argument, String argumentName)
+	{
+		Argument.notNull(argumentName, "argumentName");
+
+		if (argument < 0)
+			throw new NegativeArgumentException(argumentName);
+
+		return argument;
+	}
+
+	public static int notNegative(int argument, String argumentName)
+	{
+		Argument.notNull(argumentName, "argumentName");
+
+		if (argument < 0)
+			throw new NegativeArgumentException(argumentName);
+
+		return argument;
+	}
+
+	public static long notNegative(long argument, String argumentName)
+	{
+		Argument.notNull(argumentName, "argumentName");
+
+		if (argument < 0)
+			throw new NegativeArgumentException(argumentName);
+
+		return argument;
+	}
+
 	public static <T> T notNull(T argument, String argumentName)
 	{
 		if (argument == null)
